08ac86045d1e062ab0498e603ccc029d9ee3db2d
Yash Raut 05 <131888738+yashraut05@users.noreply.github.com>
2023-05-12 20:07:37 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char char1='X';
+    char char2='M';
+    char char3='L';
+
+    cout << "The Reverse Of: "<<char1<< char2 <<char3 <<" "<<char3<<char2<<char1<< endl;
+    return 0;
+}
